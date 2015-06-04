b774de98a4ac50366113ea31730c54f7395986d7
Ubuntu <ubuntu@ip-172-31-60-111.ec2.internal>
2015-06-04 20:17:56 +0000

    Add initial file of sql
    Author: Lu Cao <lucy.cao1989@gmail.com>
    Review: Qing Xu <xuqing.work@gmail.com>

---
+++ sql/table_init.sql
@@ -0,0 +1,110 @@
+DROP DATABASE vizisells;
+CREATE DATABASE vizisells;
+USE vizisells;
+
+
+create table vz_countries(
+country_id INT NOT NULL,
+country_name VARCHAR(256),
+PRIMARY KEY(country_id)
+);
+
+create table vz_states(
+state_id INT NOT NULL,
+state_name VARCHAR(256),
+country_id INT,
+PRIMARY KEY(state_id),
+FOREIGN KEY(country_id) REFERENCES vz_countries(country_id)
+);
+
+create table vz_cities(
+city_id INT NOT NULL,
+city_name VARCHAR(256),
+state_id INT,
+country_id INT,
+PRIMARY KEY(city_id),
+FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_states(state_id)
+);
+
+create table vz_zip(
+zip_id INT NOT NULL,
+zipcode VARCHAR(10),
+city_id INT,
+state_id INT,
+country_id INT,
+PRIMARY KEY(zip_id),
+FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_cities(city_id)
+);
+
+create table vz_users(
+user_id INT NOT NULL,
+user_password VARCHAR(512),
+email VARCHAR(256),
+user_name VARCHAR(128),
+user_flag INT,
+account_tpye INT,
+address VARCHAR(512),
+country_id INT,
+state_id INT,
+city_id INT,
+zip_id INT,
+PRIMARY KEY(user_id),
+FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_cities(city_id),
+FOREIGN KEY(zip_id) REFERENCES vz_zip(zip_id)
+);
+
+create table vz_categories(
+category_id INT NOT NULL,
+category_name VARCHAR(256),
+post_table_name VARCHAR(256),
+PRIMARY KEY(category_id)
+);
+
+create table vz_user_post_relation(
+post_id INT NOT NULL,
+category_id INT,
+user_id INT,
+PRIMARY KEY(post_id), 
+FOREIGN KEY(category_id) REFERENCES vz_categories(category_id),
+FOREIGN KEY(user_id) REFERENCES vz_users(user_id)
+);
+
+create table vz_post_merchandise(
+post_id INT,
+post_flag INT,
+auth_id INT,
+post_time TIMESTAMP,
+update_time TIMESTAMP,
+expire_time TIMESTAMP,
+post_title VARCHAR(512),
+post_head VARCHAR(1024),
+post_body VARCHAR(2048),
+post_thumb VARCHAR(512),
+post_address VARCHAR(512),
+country_id INT,
+state_id INT,
+city_id INT,
+zip_id INT,
+FOREIGN KEY(post_id) REFERENCES vz_user_post_relation(post_id),
+FOREIGN KEY(auth_id) REFERENCES vz_users(user_id),
+FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_cities(city_id),
+FOREIGN KEY(zip_id) REFERENCES vz_zip(zip_id)
+);
+
+create table vz_post_img(
+post_id INT,
+img_path VARCHAR(512),
+FOREIGN KEY(post_id) REFERENCES vz_user_post_relation(post_id)
+);
+
+
+
+
+
