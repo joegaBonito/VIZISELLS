5e04730698f7f8b875a64cbc9a70d59b517fbf3e
Ubuntu <ubuntu@ip-172-31-60-111.ec2.internal>
2015-06-05 18:38:31 +0000

    Add State and country initial SQL file
    Lu Cao <lucy.cao1989@gmail.com>
    Review: Qing Xu <xuqing.work@gmail.com>

--- sql/table_init.sql
+++ sql/table_init.sql
@@ -3,28 +3,29 @@ CREATE DATABASE vizisells;
 USE vizisells;
 
 
-create table vz_countries(
+create table vz_country(
 country_id INT NOT NULL,
 country_name VARCHAR(256),
 PRIMARY KEY(country_id)
 );
 
-create table vz_states(
+create table vz_state(
 state_id INT NOT NULL,
+state_abbr CHAR(10),
 state_name VARCHAR(256),
 country_id INT,
 PRIMARY KEY(state_id),
-FOREIGN KEY(country_id) REFERENCES vz_countries(country_id)
+FOREIGN KEY(country_id) REFERENCES vz_country(country_id)
 );
 
-create table vz_cities(
+create table vz_city(
 city_id INT NOT NULL,
 city_name VARCHAR(256),
 state_id INT,
 country_id INT,
 PRIMARY KEY(city_id),
-FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
-FOREIGN KEY(state_id) REFERENCES vz_states(state_id)
+FOREIGN KEY(country_id) REFERENCES vz_country(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_state(state_id)
 );
 
 create table vz_zip(
@@ -34,12 +35,12 @@ city_id INT,
 state_id INT,
 country_id INT,
 PRIMARY KEY(zip_id),
-FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
-FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
-FOREIGN KEY(city_id) REFERENCES vz_cities(city_id)
+FOREIGN KEY(country_id) REFERENCES vz_country(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_state(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_city(city_id)
 );
 
-create table vz_users(
+create table vz_user(
 user_id INT NOT NULL,
 user_password VARCHAR(512),
 email VARCHAR(256),
@@ -52,13 +53,13 @@ state_id INT,
 city_id INT,
 zip_id INT,
 PRIMARY KEY(user_id),
-FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
-FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
-FOREIGN KEY(city_id) REFERENCES vz_cities(city_id),
+FOREIGN KEY(country_id) REFERENCES vz_country(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_state(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_city(city_id),
 FOREIGN KEY(zip_id) REFERENCES vz_zip(zip_id)
 );
 
-create table vz_categories(
+create table vz_category(
 category_id INT NOT NULL,
 category_name VARCHAR(256),
 post_table_name VARCHAR(256),
@@ -70,8 +71,8 @@ post_id INT NOT NULL,
 category_id INT,
 user_id INT,
 PRIMARY KEY(post_id), 
-FOREIGN KEY(category_id) REFERENCES vz_categories(category_id),
-FOREIGN KEY(user_id) REFERENCES vz_users(user_id)
+FOREIGN KEY(category_id) REFERENCES vz_category(category_id),
+FOREIGN KEY(user_id) REFERENCES vz_user(user_id)
 );
 
 create table vz_post_merchandise(
@@ -83,7 +84,7 @@ update_time TIMESTAMP,
 expire_time TIMESTAMP,
 post_title VARCHAR(512),
 post_head VARCHAR(1024),
-post_body VARCHAR(2048),
+post_body TEXT,
 post_thumb VARCHAR(512),
 post_address VARCHAR(512),
 country_id INT,
@@ -91,10 +92,10 @@ state_id INT,
 city_id INT,
 zip_id INT,
 FOREIGN KEY(post_id) REFERENCES vz_user_post_relation(post_id),
-FOREIGN KEY(auth_id) REFERENCES vz_users(user_id),
-FOREIGN KEY(country_id) REFERENCES vz_countries(country_id),
-FOREIGN KEY(state_id) REFERENCES vz_states(state_id),
-FOREIGN KEY(city_id) REFERENCES vz_cities(city_id),
+FOREIGN KEY(auth_id) REFERENCES vz_user(user_id),
+FOREIGN KEY(country_id) REFERENCES vz_country(country_id),
+FOREIGN KEY(state_id) REFERENCES vz_state(state_id),
+FOREIGN KEY(city_id) REFERENCES vz_city(city_id),
 FOREIGN KEY(zip_id) REFERENCES vz_zip(zip_id)
 );
 
