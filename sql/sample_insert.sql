d9480274d86e03d3402b33d27edfb3916d6ddc52
Ubuntu <ubuntu@ip-172-31-49-197.ec2.internal>
2015-07-13 20:22:28 +0000

    Add thumbs link to post sample, images stored in folder 'thumb'
    Lucy<lucy.cao1989@gmail.com>

--- sql/sample_insert.sql
+++ sql/sample_insert.sql
@@ -45,13 +45,12 @@ Origin: China
 ASIN: B00DR69O0G
 Item model number: W016B-P
 Average Customer Review: 4.5 out of 5 stars  See all reviews (111 customer reviews)
-Amazon Best Sellers Rank: #126 in Sports & Outdoors (See Top 100 in Sports & Outdoors)',null,'Castle Point on Hudson',112190,35,1,122892);
+Amazon Best Sellers Rank: #126 in Sports & Outdoors (See Top 100 in Sports & Outdoors)','../thumb/cup.jpg','Castle Point on Hudson',112190,35,1,122892);
 
 INSERT INTO vz_post_merchandise VALUES
 (10000001,1,100001,now(),now(),ADDTIME(post_time, '30 0:0:0'),
 'Ipow Magic Porcelain Ceramic Large Office Speak Writing Message/memo Postcard Milk Tea Soup Coffee Mug Cup',
-'Tall, Sturdy and Larger 15 oz mug is well made and felt solid in your hands,enough for a real cup of, well, tea or coffee.
-Fun, Cute and Personalized design,come with lined area, black wipe, pencil, and pencil stand',
+'Tall, Sturdy and Larger 15 oz mug is well made and felt solid in your hands,enough for a real cup of, well, tea or coffee.',
 'Postcard coffee mug can be used for home and office. 
 You can write any kind word for your family at any time when you want. 
 You can write any meeting time or another that you are in meeting. 
@@ -60,8 +59,8 @@ Because Ipow have matched black cloth to scrub it with water,Easy to wash. NOTE
 1. Do not expose under the sunshine for long time. 
 2. Do not scrape on the printing with sharp tools. 
 3. Easy to wash. 
-5. Do not scald yourself. ',
-null,'863 W Jericho Tpke',124761,37,1,125430);
+5. Do not scald yourself.' ,
+'../thumb/knife.jpg','863 W Jericho Tpke',124761,37,1,125430);
 
 
 INSERT INTO vz_post_merchandise VALUES
@@ -75,7 +74,7 @@ Shipping Weight: 1.5 pounds (View shipping rates and policies)
 Shipping: This item is also available for shipping to select countries outside the U.S.
 Origin: El Salvador
 Item model number: S2468',
-null,'204 5th St',113274,35,1,122887);
+'../thumb/Stationery.png','204 5th St',113274,35,1,122887);
 
 
 INSERT INTO vz_post_merchandise VALUES
@@ -99,7 +98,7 @@ Meets EN71 European Safety Standards
 
 
 Recommended Ages:3 ¨C 5',
-null,'13558 Lefferts Blvd',125010,37,1,125362);
+'../thumb/toy.png','13558 Lefferts Blvd',125010,37,1,125362);
 
 INSERT INTO vz_post_merchandise VALUES
 (10000004,1,100004,now(),now(),ADDTIME(post_time, '30 0:0:0'),
@@ -111,4 +110,4 @@ Domestic Shipping: Item can be shipped within U.S.
 International Shipping: This item can be shipped to select countries outside of the U.S. Learn More
 ASIN: B009MRLOWK
 Item model number: 07281',
-null,'71 E 116th St',118847,37,1,125114);
+'../thumb/umbrella.png','71 E 116th St',118847,37,1,125114);
