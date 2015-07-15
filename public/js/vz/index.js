eef9c181cd9b8cef73bd1f5ed62b933be764d758
joegabonito <joejung5678@gmail.com>
2015-07-15 19:50:19 +0000

    Layout Bar fixed. Size stays the same.
    Joe Jung <joejung5678@gmail.com>

--- public/js/vz/index.js
+++ public/js/vz/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 /* Hovering Effect */
 	$(".categoryDiv").mouseenter(function() {
 		$(this).children("p").fadeTo("fast","1");
-		$(this).find("img").fadeTo("fast",".2");
+		$(this).find("img").fadeTo("fast",".1");
 	}).mouseleave(function() {
 		$(this).children("p").fadeTo("fast","0");
 		$(this).find("img").fadeTo("fast","1");
