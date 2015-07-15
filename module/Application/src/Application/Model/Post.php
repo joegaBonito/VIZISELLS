be933e60073de862f48afcde1fd84f711ffed79e
Ubuntu <ubuntu@ip-172-31-52-203.ec2.internal>
2015-07-15 20:19:09 +0000

    Add thumbs of post list
    Qing Xu <xuqing.work@gmail.com>

--- module/Application/src/Application/Model/Post.php
+++ module/Application/src/Application/Model/Post.php
@@ -5,5 +5,6 @@ class Post {
     public function exchangeArray($data) {
         $this->post_title = (!empty($data['post_title'])) ? $data['post_title'] : null;
         $this->post_head = (!empty($data['post_head'])) ? $data['post_head'] : null;
+        $this->post_thumb = (!empty($data['post_thumb'])) ? $data['post_thumb'] : null;
     }
 }
