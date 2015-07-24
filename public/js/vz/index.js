f936e6eb74fad1ceb1152929352e4c4c4dc1919f
joegabonito <joejung5678@gmail.com>
2015-07-24 19:35:20 +0000

    Added Bullet Points
    Joe Jung <joejung5678@gmail.com>

--- public/js/vz/index.js
+++ public/js/vz/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 /* Hovering Effect */
 	$(".categoryDiv").mouseenter(function() {
-		$(this).children("p").fadeTo("fast","1");
+		$(this).find("p,ul").fadeTo("fast","1");
 		$(this).find("img").fadeTo("fast",".1");
 	}).mouseleave(function() {
-		$(this).children("p").fadeTo("fast","0");
+		$(this).find("p,ul").fadeTo("fast","0");
 		$(this).find("img").fadeTo("fast","1");
 	});
 });
